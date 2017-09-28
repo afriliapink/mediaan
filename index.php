c029ef65f5e79eece14007bc1903e65e1e654aba
afriliapink <afrilia04@outlook.com>
2017-09-28 20:24:18 +0700

    versi 1.0

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+	<title> Resume Magali Dubourdieu </title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="banner-wrapper">
+		<div class="color-overlay"></div>
+		<figure id="thumbnail">
+			<img src="bomi.jpg" alt="Writing Machine"/>
+		</figure>
+		<div class="desc-wrapper">
+			<p id="p-banner"><em>All I Wanna Do, Wanna One</em></p>
+			<p id="par-banner">MEDIAAN</p>
+		</div>
+	</div>
+</html>
+
+
